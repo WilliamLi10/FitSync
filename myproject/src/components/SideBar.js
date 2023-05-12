25e35ae3d0e77f34e535a5a823c7a7c80db58f60
WilliamLi10 <william3ny@gmail.com>
2023-05-12 01:11:50 -0400

    Continued adding to layout

--- myproject/src/components/SideBar.js
+++ myproject/src/components/SideBar.js
@@ -1,7 +1,7 @@
 const SideBar = () => {
     return (
         <div className="h-screen w-1/6 border-r-[2px] border-black border-solid text-center">
-            <h1 className="text-xl">Current Plan</h1>
+            <h1 className="text-xl my-4">Current Plan</h1>
         </div>
 
     );
