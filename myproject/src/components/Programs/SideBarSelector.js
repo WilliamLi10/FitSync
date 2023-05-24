90794971af4b7451d071640cbee30f3f532e0922
WilliamLi10 <william3ny@gmail.com>
2023-05-23 20:40:46 -0400

    Even More Stuff

--- myproject/src/components/Programs/SideBarSelector.js
+++ myproject/src/components/Programs/SideBarSelector.js
@@ -1,6 +1,6 @@
 const SideBarSelector = (props) =>{
     return (
-        <div>
+        <div className="text-white display flex flex-row">
             {props.image}
             <h1>{props.desc}</h1>
         </div>
