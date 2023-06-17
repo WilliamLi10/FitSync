66e3ad24ee81356d2fa0d89270dc4497317b8ae6
Jia <jiahuang2468@gmail.com>
2023-06-17 04:03:56 -0400

    updated workout modal

--- FrontEnd/src/components/MainPage/Exercise.js
+++ FrontEnd/src/components/MainPage/Exercise.js
@@ -9,7 +9,10 @@ const Exercise = (props) => {
 
   return (
     <div className="my-10 text-lg">
-      <p onClick={dropHandler}>{props.exercise.Name}</p>
+      <p onClick={dropHandler}>
+        <i className="border-solid border-black border-t-[3px] border-r-[3px] border-l-[0px] border-b-[0px] p-[3px] rotate-[135deg] inline-block"></i>
+        {props.exercise.Name}
+      </p>
       {drop && (
         <div>
           {props.exercise.Description && <p>{props.exercise.Description}</p>}
