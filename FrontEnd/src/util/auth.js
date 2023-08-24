4e240caf50cb20a9447c930b65d4b99eebaeb9cf
Jia <jiahuang2468@gmail.com>
2023-08-24 04:11:11 -0400

    changed refresh token exp back to 1 hr

--- FrontEnd/src/util/auth.js
+++ FrontEnd/src/util/auth.js
@@ -58,7 +58,7 @@ export const refreshToken = () => {
           });
 
           const expirationDate1Hour = new Date();
-          expirationDate1Hour.setHours(expirationDate1Hour.getHours() + 15);
+          expirationDate1Hour.setHours(expirationDate1Hour.getHours() + 1);
 
           Cookies.set("refreshToken", data.refreshToken, {
             expires: expirationDate1Hour,
