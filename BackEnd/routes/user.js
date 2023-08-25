b2b9a6a830810c7219a5fee4bca16f6e77b6256c
WilliamLi10 <william3ny@gmail.com>
2023-08-25 01:54:57 -0400

    Get User Endpoint

--- BackEnd/routes/user.js
+++ BackEnd/routes/user.js
@@ -11,10 +11,31 @@ router.get("/search-user", verifyAccessToken, (req, res) => {
     .getUserByUsername(username)
     .then((userExists) => {
       console.log("Searched user successfully");
+      res.status(200);
       if (!userExists.exists) {
         return res.json({ success: userExists.exists });
       } else {
         return res.json({ success: userExists.exists, user: userExists.user });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+router.get("/get-user", verifyAccessToken, (req, res) => {
+  console.log("+++");
+  console.log("Getting user...");
+  const username = req.query.username;
+  new Users()
+    .getUserByUsername(username)
+    .then((user) => {
+      if (!user.exists) {
+        res.status(404).json({ error: "User Not Found" });
+      } else {
+        res.status(200);
+        return res.json(user.user);
       }
     })
     .catch((error) => {
