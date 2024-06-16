9d6cc3b84ce09ae506b13b4ea91152012ef668f6
Jia <jiahuang2468@gmail.com>
2024-06-15 22:18:27 -0400

    added edit icon

---
+++ FrontEnd/src/components/Programs/MyPrograms/ProgramList/EditProgram.js
@@ -0,0 +1,11 @@
+const EditProgram = () => {
+  return (
+    <div className="absolute bg-white shadow-md top-[100%] z-10">
+      <div className="hover:bg-gray-50 px-3 py-2">Rename</div>
+      <div className="hover:bg-gray-50 px-3 py-2">Delete</div>
+      <div className="hover:bg-gray-50 px-3 py-2">Open in New Tab</div>
+    </div>
+  );
+};
+
+export default EditProgram;
--- FrontEnd/src/components/Programs/MyPrograms/ProgramList/ProgramItem.js
+++ FrontEnd/src/components/Programs/MyPrograms/ProgramList/ProgramItem.js
@@ -1,12 +1,22 @@
 import { useNavigate } from "react-router-dom";
+import { SlOptionsVertical } from "react-icons/sl";
+import EditProgram from "./EditProgram";
+import { useState } from "react";
 
 const ProgramItem = (props) => {
   const navigate = useNavigate();
   const date = new Date(props.program.lastOpened);
 
+  const [edit, setEdit] = useState(false);
+
   const month = date.toLocaleString("default", { month: "short" });
   const day = date.getDate();
   const year = date.getFullYear();
+
+  const editHandler = (event) => {
+    event.stopPropagation();
+    setEdit(true);
+  };
 
   return (
     <div
@@ -24,6 +34,11 @@ const ProgramItem = (props) => {
       <div className="w-[20%] overflow-hidden overflow-ellipsis whitespace-nowrap pr-3">
         {`${month} ${day}, ${year}`}
       </div>
+      <SlOptionsVertical
+        className="transition-all duration-150 hover:bg-gray-300 rounded-full py-1 h-5 w-5"
+        onClick={editHandler}
+      />
+      {edit && <EditProgram />}
     </div>
   );
 };
