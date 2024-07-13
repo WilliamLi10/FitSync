c8dde1e14b7cdbbe652c41f89dc0bdb5fa4d2508
Jia Huang <jiahuang2468@gmail.com>
2024-07-13 16:40:21 -0400

    68 delete sidebar and discover programs (#70)
    * deleted discover program
    
    * deleted side bar
    
    * Update Programs.js
    
    ---------
    
    Co-authored-by: WilliamLi10 <93461685+WilliamLi10@users.noreply.github.com>

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,8 +1,4 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./pages/Root";
 import Error from "./pages/Error";
 import "tailwindcss/tailwind.css";
@@ -10,9 +6,9 @@ import Home from "./pages/Home";
 import Programs from "./pages/LoggedIn/Programs";
 import Progress from "./pages/LoggedIn/Progress";
 import LogWorkout from "./pages/LoggedIn/LogWorkout";
-import DiscoverPrograms from "./components/Programs/DiscoverPrograms/DiscoverPrograms";
-import MyPrograms from "./components/Programs/MyPrograms/MyPrograms";
-import ProgramView from "./components/Programs/MyPrograms/ProgramView/ProgramView";
+import Product from "./pages/LoggedOut/Product";
+import Contact from "./pages/LoggedOut/Contact";
+import ProgramView from "./components/Programs/ProgramView/ProgramView";
 import { programLoader } from "./loaders/ProgramLoader";
 
 const router = createBrowserRouter([
@@ -24,22 +20,11 @@ const router = createBrowserRouter([
       { path: "/", element: <Home /> },
       { path: "progress", element: <Progress /> },
       { path: "logworkout", element: <LogWorkout /> },
+      { path: "programs", element: <Programs /> },
       {
-        path: "programs",
-        element: <Programs />,
-        children: [
-          { index: true, element: <Navigate to="myprograms" replace /> },
-          { path: "discover", element: <DiscoverPrograms /> },
-          {
-            path: "myprograms",
-            element: <MyPrograms />,
-          },
-          {
-            path: "/programs/myprograms/:programID",
-            element: <ProgramView />,
-            loader: programLoader,
-          },
-        ],
+        path: "programs/:programID",
+        element: <ProgramView />,
+        loader: programLoader,
       },
     ],
   },
