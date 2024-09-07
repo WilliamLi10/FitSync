0c51869cd24ba4ac354e97d8978fbff6e7c85e58
WilliamLi10 <93461685+WilliamLi10@users.noreply.github.com>
2024-09-07 14:57:44 -0400

    Deployment changes (#92)
    * Deployment changes
    
    * Deployment Changes
    
    ---------
    
    Co-authored-by: William Li <williamli@Williams-Air.lan>

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -20,7 +20,7 @@ connectDB()
     console.log("Connected to Redis");
   })
   .then(() => {
-    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+    app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
     app.use(express.json());
     app.use(passport.initialize());
 
